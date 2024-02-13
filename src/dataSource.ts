da87c5596e6c2952f7727877352528829596e560
devaku <alejokimuy@gmail.com>
2024-02-13 11:51:28 +0800

    Updated data source

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,19 +1,31 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { Post } from './entity/Post';
-import { User } from './entity/User';
 
-export const AppDataSource = new DataSource({
-	type: 'mysql',
+const connToDS = async () => {
+	const dataSourceConn = new DataSource({
+		type: 'mysql',
 
-	// ENV
-	host: 'localhost',
-	port: 3306,
-	username: 'root',
-	password: '',
-	database: 'test',
+		// ENV
+		host: process.env.DB_HOST,
+		port: Number(process.env.DB_PORT),
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
 
-	entities: [Post, User],
-	synchronize: true,
-	logging: false,
-});
+		entities: [`${__dirname}/entity/*.{j,t}s`],
+		migrations: [`${__dirname}/migrations/*.{j,t}s`],
+		synchronize: true,
+		logging: false,
+	});
+	try {
+		await dataSourceConn.initialize();
+		console.log('Data Source has been initialized!');
+
+		return dataSourceConn;
+	} catch (err) {
+		console.error('Error during Data Source initialization', err);
+		throw err;
+	}
+};
+
+export const dataSource = connToDS();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,66 @@
-import { AppDataSource } from './dataSource';
+import 'dotenv/config';
+import { dataSource } from './dataSource';
+
 import { User } from './entity/User';
 
-AppDataSource.initialize()
-	.then(async () => {
-		const Users = await AppDataSource.manager.find(User);
 
-		console.log(Users);
-	})
-	.catch((e) => console.log(e));
+
+async function testQuery() {
+	// Connect to the database
+	let serviceDS = await dataSource;
+
+	// Interact with database
+	const Users = await serviceDS.manager.find(User);
+	console.log(Users);
+}
+
+async function transactionTest() {
+	// Connect to the database
+	let serviceDS = await dataSource;
+
+	// Interact with database
+
+	const Users = await serviceDS.transaction(
+		async (transactionalEntityManager) => {
+			const user = new User();
+			user.firstName = 'TRANSACTION FIRST';
+			user.lastName = 'Saw';
+			user.age = 25;
+
+			await transactionalEntityManager.save(user);
+
+			let rows = await transactionalEntityManager.find(User);
+			return rows;
+		}
+	);
+
+	console.log(Users);
+}
+
+async function queryBuilder() {
+	let serviceDS = await dataSource;
+	// const user = await serviceDS.manager
+	// 	.createQueryBuilder(User, 'user')
+	// 	.where('user.id = :id', { id: 1 })
+	// 	.getQueryAndParameters();
+
+	// console.log(user);
+
+	const qb = serviceDS.manager.createQueryBuilder(User, 'user');
+
+	let determiner = 4;
+
+	if (determiner == 1) {
+		qb.where('user.id = :id', { id: 1 });
+	} else {
+		qb.where('user.id = :id', { id: 4 });
+	}
+
+	let rows = await qb.getOne();
+
+	console.log(rows);
+}
+
+// transactionTest();
+
+// queryBuilder();
